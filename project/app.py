31fd2c06be5e19b9bd0ca3eb4eccedfd86cf9fa9
robinsam1 <samuel.robinson@mail.mcgill.ca>
2017-06-17 18:06:33 +0100

    Created form where user can enter number

--- project/app.py
+++ project/app.py
@@ -1,14 +1,18 @@
-from flask import Flask,render_template
+from flask import Flask,request,render_template
 from calculator import calculator
 calculator = calculator()
 application = Flask(__name__)
 
 @application.route('/')
-def showMachineList():
-     #return render_template('list.html')
-     input = raw_input("Choose a number")
-     x = calculator.calculate(input)
-     return "%.2f" %x
+def my_form():
+    return render_template("my-form.html")
+
+@application.route('/', methods=['POST'])
+def my_form_post():
+
+    text = request.form['text']
+    x = calculator.calculate(float(text))
+    return "%.2f" %x
 
 if __name__ == "__main__":
      application.run(host='0.0.0.0')
