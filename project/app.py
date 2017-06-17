a7a0310484ec03e6c695c716851d0b580d0af6d2
robinsam1 <samuel.robinson@mail.mcgill.ca>
2017-06-17 15:52:47 +0100

    Added calculator class and function call

--- project/app.py
+++ project/app.py
@@ -1,9 +1,13 @@
 from flask import Flask,render_template
+from calculator import calculator
+calculator = calculator()
 application = Flask(__name__)
 
 @application.route('/')
 def showMachineList():
-     return render_template('list.html')
+     #return render_template('list.html')
+     x = calculator.calculate(0)
+     return "%.2f" %x
 
 if __name__ == "__main__":
      application.run(host='0.0.0.0')
---
+++ project/calculator.py
@@ -0,0 +1,8 @@
+class calculator(object):
+	def __init__(self):
+		self.x = 'Calculator'
+
+	def calculate(self, answers):
+		score = 10.0
+		#for answer in answers:
+		return score
